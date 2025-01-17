59d05e41bbc88d86de7651e3f84920cd6ac500ff
unknown <amit.singh@brainiuminfotech.com>
2025-01-17 23:11:15 +0530

    Project Setup

---
+++ App/Navigation/StackNavigation.js
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const StackNavigation = () => {
+  return (
+    <View>
+      <Text>StackNavigation</Text>
+    </View>
+  );
+};
+
+export default StackNavigation;
